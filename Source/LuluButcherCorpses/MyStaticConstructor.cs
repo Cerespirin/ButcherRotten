72cb986ac0b5cd06976a8cb3c278fe8534139860
Cerespirin <151862361+Cerespirin@users.noreply.github.com>
2023-12-21 07:19:40 -0500

    Initial commit

---
+++ Source/LuluButcherCorpses/MyStaticConstructor.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Verse;
+
+#pragma warning disable IDE1006 // Naming Styles
+
+namespace LoonyLadle.ButcherRotten
+{
+	[StaticConstructorOnStartup]
+	public static class MyStaticConstructor
+	{
+		static MyStaticConstructor()
+		{
+			Harmony harmony = new Harmony("rimworld.loonyladle.butcherrotten");
+			harmony.PatchAll();
+		}
+	}
+}
